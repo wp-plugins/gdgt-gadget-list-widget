852b494e2bfd0fa678e8885ee29ab0592b5a5742
gdgt <gdgt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-30 04:58:36 +0000

    WordPress coding standards. http://codex.wordpress.org/WordPress_Coding_Standards
    git-svn-id: https://plugins.svn.wordpress.org/gdgt-gadget-list-widget/trunk@417099 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gdgt-gadget-list.php
+++ gdgt-gadget-list.php
@@ -9,71 +9,69 @@ Author URI: http://gdgt.com/
 License: GPLv2 or later
 */
 
-define('GDGT_HOST', 'gdgt.com');
-define('GDGT_CDN_URL', 'http://media.gdgt.com/');
-define('GDGT_URL', 'http://' . GDGT_HOST);
-define('GDGT_API_URL', 'http://api.' . GDGT_HOST);
-define('GDGT_USR_URL', 'http://user.' . GDGT_HOST);
-define('GDGT_IMG_URL', GDGT_CDN_URL . 'img/');
-define('GDGT_CSS_URL', GDGT_CDN_URL . 'css/');
-define('GDGT_REFRESH_LIST', (60 * 60 * 24));
+define( 'GDGT_HOST', 'gdgt.com' );
+define( 'GDGT_CDN_URL', 'http://media.gdgt.com/' );
+define( 'GDGT_URL', 'http://' . GDGT_HOST );
+define( 'GDGT_API_URL', 'http://api.' . GDGT_HOST );
+define( 'GDGT_USR_URL', 'http://user.' . GDGT_HOST );
+define( 'GDGT_IMG_URL', GDGT_CDN_URL . 'img/' );
+define( 'GDGT_CSS_URL', GDGT_CDN_URL . 'css/' );
+define( 'GDGT_REFRESH_LIST', (60 * 60 * 24) );
 
 /**
  * Load CSS and JS
  */
 function gdgt_gadget_list_init() {
-  if (is_active_widget(false, false, 'gadgetlistwidget')) {
-    wp_enqueue_script('gdgt-gadget-list-js', plugins_url('gdgt-gadget-list.js', __FILE__));
-    wp_enqueue_style('gdgt-gadget-list-css', plugins_url('gdgt-gadget-list.css', __FILE__), null, null, 'all');
-  }
+	if ( is_active_widget( false, false, 'gadgetlistwidget' ) ) {
+		wp_enqueue_script( 'gdgt-gadget-list-js', plugins_url( 'gdgt-gadget-list.js', __FILE__ ) );
+		wp_enqueue_style( 'gdgt-gadget-list-css', plugins_url( 'gdgt-gadget-list.css', __FILE__ ), null, null, 'all' );
+	}
 }
-add_action('wp_enqueue_scripts', 'gdgt_gadget_list_init');
+add_action( 'wp_enqueue_scripts', 'gdgt_gadget_list_init' );
 
 class GadgetListWidget extends WP_Widget {
 
-  function __construct() {
-    parent::__construct(false, 'gdgt Gadget List', array('description' => 'Show off your gadgets!'));
-  }
+	function __construct() {
+		parent::__construct( false, 'gdgt Gadget List', array( 'description' => 'Show off your gadgets!' ) );
+	}
 
-  function GadgetListWidget() {
-    $this->__construct();
-  }
+	function GadgetListWidget() {
+		$this->__construct();
+	}
 
-  function widget($args, $instance) {
-    extract($args);
-    echo $before_widget;
-    $options = array();
-    $options['width'] = $instance['width'] ? $instance['width'] : 180;
-    $options['height'] = $instance['height'] ? $instance['height'] : 250;
-    $options['showcount'] = $instance['showcount'];
+	function widget($args, $instance) {
+		extract($args);
+		echo $before_widget;
+		$options = array();
+		$options['width'] = $instance['width'] ? $instance['width'] : 180;
+		$options['height'] = $instance['height'] ? $instance['height'] : 250;
+		$options['showcount'] = $instance['showcount'];
 
-    if (isset($instance['gdgtuser'])) {
-      gdgt_gadget_list_widget($instance['gdgtuser'], $options);
-    }
-    echo $after_widget;
-  }
+		if ( isset( $instance['gdgtuser'] ) )
+			gdgt_gadget_list_widget( $instance['gdgtuser'], $options );
+		echo $after_widget;
+	}
 
-  function form($instance) {
-    ?>
+	function form($instance) { ?>
     <div>
-      <p><label for="<?php echo esc_attr($this->get_field_id('gdgtuser')); ?>"><?php echo sprintf(__('%s username:', 'gdgt-gadget-list'), 'gdgt'); ?></label></p>
-      <p><input type="text" class="widefat" id="<?php echo esc_attr($this->get_field_id('gdgtuser')); ?>" name="<?php echo esc_attr($this->get_field_name('gdgtuser')); ?>" value="<?php if (isset($instance['gdgtuser'])) {
-      echo esc_attr($instance['gdgtuser']);
+      <p><label for="<?php echo esc_attr( $this->get_field_id( 'gdgtuser' ) ); ?>"><?php echo sprintf( __( '%s username:', 'gdgt-gadget-list' ), 'gdgt' ); ?></label></p>
+      <p><input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'gdgtuser' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'gdgtuser' ) ); ?>" value="<?php if ( isset( $instance['gdgtuser'] ) ) {
+      echo esc_attr( $instance['gdgtuser'] );
     } ?>" /></p>
     </div>
     <div>
       <p>
-        <label for="<?php echo esc_attr($this->get_field_id('width')); ?>"><?php echo __('width:', 'gdgt-gadget-list'); ?></label>
-        <input type="text" id="<?php echo esc_attr($this->get_field_id('width')); ?>" name="<?php echo esc_attr($this->get_field_name('width')); ?>" value="<?php if (isset($instance['width'])) {
-      echo absint($instance['width']);
+        <label for="<?php echo esc_attr( $this->get_field_id( 'width' ) ); ?>"><?php echo __( 'width:', 'gdgt-gadget-list' ); ?></label>
+        <input type="text" id="<?php echo esc_attr( $this->get_field_id( 'width' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'width' ) ); ?>" value="<?php if ( isset( $instance['width'] ) ) {
+      echo absint( $instance['width'] );
     } else {
       echo 200;
     } ?>" style="width: 40px;" />px
       </p>
       <p>
-        <label for="<?php echo esc_attr($this->get_field_id('height')); ?>"><?php echo __('height:', 'gdgt-gadget-list'); ?></label>
-        <input type="text" id="<?php echo esc_attr($this->get_field_id('height')); ?>" name="<?php echo esc_attr($this->get_field_name('height')); ?>" value="<?php if (isset($instance['height'])) {
-      echo absint($instance['height']);
+        <label for="<?php echo esc_attr( $this->get_field_id( 'height' ) ); ?>"><?php echo __( 'height:', 'gdgt-gadget-list' ); ?></label>
+        <input type="text" id="<?php echo esc_attr($this->get_field_id('height')); ?>" name="<?php echo esc_attr( $this->get_field_name( 'height' ) ); ?>" value="<?php if ( isset( $instance['height'] ) ) {
+      echo absint( $instance['height'] );
     } else {
       echo 300;
     } ?>" style="width: 40px;" />px
@@ -81,22 +79,22 @@ class GadgetListWidget extends WP_Widget {
     </div>
     <div>
       <p>
-    <?php echo __('show list counts:', 'gdgt-gadget-list'); ?> <input type="radio" id="<?php echo esc_attr($this->get_field_id('showcount') . '_true'); ?>" name="<?php echo esc_attr($this->get_field_name('showcount')); ?>" value="true" <?php if (isset($instance['showcount']) && ( $instance['showcount'] == 'true' || empty($instance['showcount'])))
-      echo 'checked="checked"'; ?> /> <label for="<?php echo esc_attr($this->get_field_id('showcount') . '_true'); ?>"><?php echo __('yes', 'gdgt-gadget-list'); ?></label> <input type="radio" id="<?php echo esc_attr($this->get_field_id('showcount') . '_false'); ?>" name="<?php echo esc_attr($this->get_field_name('showcount')); ?>" value="false" <?php if (isset($instance['showcount']) && ($instance['showcount'] == 'false'))
-      echo 'checked="checked"'; ?> /> <label for="<?php echo esc_attr($this->get_field_id('showcount') . '_false'); ?>"><?php echo __('no', 'gdgt-gadget-list'); ?></label>
+	<?php echo __( 'show list counts:', 'gdgt-gadget-list' ); ?> <input type="radio" id="<?php echo esc_attr( $this->get_field_id( 'showcount' ) . '_true' ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'showcount' ) ); ?>" value="true" <?php if ( isset( $instance['showcount'] ) && ( $instance['showcount'] == 'true' || empty( $instance['showcount'] ) ) )
+      echo 'checked="checked"'; ?> /> <label for="<?php echo esc_attr( $this->get_field_id( 'showcount' ) . '_true'); ?>"><?php echo __( 'yes', 'gdgt-gadget-list' ); ?></label> <input type="radio" id="<?php echo esc_attr( $this->get_field_id('showcount' ) . '_false' ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'showcount' ) ); ?>" value="false" <?php if ( isset( $instance['showcount'] ) && $instance['showcount'] == 'false' )
+      echo 'checked="checked"'; ?> /> <label for="<?php echo esc_attr( $this->get_field_id( 'showcount' ) . '_false'); ?>"><?php echo __( 'no', 'gdgt-gadget-list' ); ?></label>
         <br />
-    <?php echo __('(only if width is 200px or more)', 'gdgt-gadget-list'); ?>
+	<?php echo __( '(only if width is 200px or more)', 'gdgt-gadget-list' ); ?>
       </p>
     </div><?php
   }
 
-  function update($new_instance, $old_instance) {
-    return $new_instance;
-  }
+	function update( $new_instance, $old_instance ) {
+		return $new_instance;
+	}
 
 }
 
-add_action('widgets_init', create_function('', 'return register_widget(\'GadgetListWidget\');'));
+add_action( 'widgets_init', create_function( '', 'return register_widget(\'GadgetListWidget\');' ) );
 
 /**
  * Retrieve data from gdgt api servers and display the widget.
@@ -104,38 +102,36 @@ add_action('widgets_init', create_function('', 'return register_widget(\'GadgetL
  * @param string $username gdgt username
  * @param array $options widget options
  */
-function gdgt_gadget_list_widget($username, $options=array()) {
-  $now = time();
-  $datakey = 'gdgt_widget_' . $username;
-  $timekey = $datakey . '_updated';
+function gdgt_gadget_list_widget( $username, $options=array() ) {
+	$now = time();
+	$datakey = 'gdgt_widget_' . $username;
+	$timekey = $datakey . '_updated';
 
-  $data = false;
-  
-  $data = get_transient($datakey);
-  if ($data === false) {
-    $last_updated = get_transient($timekey);
+	$data = false;
 
-    if ($last_updated === false || ($now - $last_updated) > GDGT_REFRESH_LIST) {
-      $response = wp_remote_get(GDGT_API_URL . '/profile/' . strtolower($username) . '/gadgets.jsonp');
-      if (true || !is_wp_error($response) && wp_remote_retrieve_response_code($response) == 200) {
-        $response_body = trim(wp_remote_retrieve_body($response));
-        if (empty($response_body)) {
-          return;
-        }
+	$data = get_transient( $datakey );
+	if ( $data === false ) {
+		$last_updated = get_transient( $timekey );
 
-        $data = json_decode($response_body);
-        if (empty($data) || !isset($data->user)) {
-          return;
-        }
-        
-        set_transient($datakey, $data, GDGT_REFRESH_LIST);
-        set_transient($timekey, $now, GDGT_REFRESH_LIST);
-      }
-    }
-  }
+		if ( $last_updated === false || ($now - $last_updated) > GDGT_REFRESH_LIST ) {
+			$response = wp_remote_get(GDGT_API_URL . '/profile/' . strtolower($username) . '/gadgets.jsonp');
+			if ( true || ! is_wp_error( $response ) && wp_remote_retrieve_response_code( $response ) == 200) {
+				$response_body = trim( wp_remote_retrieve_body( $response ) );
+				if ( empty( $response_body ) )
+					return;
 
-  if (!empty($data))
-    create_gadget_list_widget($username, $options, $data);
+				$data = json_decode( $response_body );
+				if ( empty( $data ) || ! isset( $data->user ) )
+					return;
+
+				set_transient( $datakey, $data, GDGT_REFRESH_LIST );
+				set_transient( $timekey, $now, GDGT_REFRESH_LIST );
+			}
+		}
+	}
+
+	if ( ! empty( $data ) )
+		create_gadget_list_widget($username, $options, $data);
 }
 
 /**
@@ -145,31 +141,31 @@ function gdgt_gadget_list_widget($username, $options=array()) {
  * @param array $options widget options
  * @param array $data widget data
  */
-function create_gadget_list_widget($username, $options=array(), $data=null) {
-  $have_count = $want_count = $had_count = 0;
-  $user_url = $user_img = '';
-  if (isset($options['showcount']) && $options['showcount'] === false) {
-    $options['showcount'] = 'false';
-    $show_counts = false;
-  } else {
-    $show_counts = true;
-  }
+function create_gadget_list_widget( $username, $options=array(), $data=null ) {
+	$have_count = $want_count = $had_count = 0;
+	$user_url = $user_img = '';
+	if ( isset( $options['showcount'] ) && $options['showcount'] === false ) {
+		$options['showcount'] = 'false';
+		$show_counts = false;
+	} else {
+		$show_counts = true;
+	}
 
-  if (!empty($data)) {
-    $user = $data->user;
-    $firstname = $user->first_name;
-    $user_url = $user->profile_url;
-    $user_img = $user->avatar_file_path;
+	if ( ! empty( $data ) ) {
+		$user = $data->user;
+		$firstname = $user->first_name;
+		$user_url = $user->profile_url;
+		$user_img = $user->avatar_file_path;
 
-    if (isset($data->have) && is_array($data->have))
-      $have_count = count($data->have);
-    if (isset($data->have) && is_array($data->want))
-      $want_count = count($data->want);
-    if (isset($data->had) && is_array($data->had))
-      $had_count = count($data->had);
-  } else {
-    $data = stdClass();
-  }
+		if ( isset( $data->have ) && is_array( $data->have ) )
+			$have_count = count( $data->have );
+		if ( isset( $data->have ) && is_array( $data->want ) )
+			$want_count = count( $data->want );
+		if ( isset( $data->had ) && is_array( $data->had ) )
+			$had_count = count( $data->had );
+	} else {
+		$data = stdClass();
+	}
 
-  include dirname(__FILE__) . '/widget-skeleton.php';
+	include dirname(__FILE__) . '/widget-skeleton.php';
 }
--- widget-skeleton.php
+++ widget-skeleton.php
@@ -1,13 +1,13 @@
 <div id="gdgt_widget_<?php echo $username; ?>" class="gdgt-gadget-list-widget">
   <div class="gdgt-widget-header">
-    <a href="<?php echo esc_url(GDGT_URL, array('http', 'https')); ?>" class="gdgt-logo" title="gdgt"></a>
-    <h4><a href="<?php echo esc_url($user_url); ?>" title="<?php echo esc_attr($username); ?> on gdgt"><?php echo __('my <strong>gadgets</strong>', 'gdgt-gadget-list-widget'); ?></a></h4>
+    <a href="<?php echo esc_url( GDGT_URL, array( 'http', 'https' ) ); ?>" class="gdgt-logo" title="gdgt"></a>
+    <h4><a href="<?php echo esc_url( $user_url ); ?>" title="<?php echo esc_attr( $username ); ?> on gdgt"><?php echo __( 'my <strong>gadgets</strong>', 'gdgt-gadget-list-widget' ); ?></a></h4>
     <ul class="tabs">
-      <li class="have selected"><a class="have" title="have (<?php echo $have_count; ?>)"><?php echo __('has', 'gdgt-gadget-list-widget'); ?> <span class="count"><?php if ($have_count > 0 && $show_counts)
+      <li class="have selected"><a class="have" title="have (<?php echo $have_count; ?>)"><?php echo __( 'has', 'gdgt-gadget-list-widget' ); ?> <span class="count"><?php if ( $have_count > 0 && $show_counts )
   echo '(' . $have_count . ')'; ?></span></a></li>
-      <li class="want"><a class="want" title="want (<?php echo $want_count; ?>)">wants <span class="count"><?php if ($want_count > 0 && $show_counts)
+      <li class="want"><a class="want" title="want (<?php echo $want_count; ?>)">wants <span class="count"><?php if ( $want_count > 0 && $show_counts )
   echo '(' . $want_count . ')'; ?></span></a></li>
-      <li class="had"><a class="had" title="had (<?php echo $had_count; ?>)">had <span class="count"><?php if ($had_count > 0 && $show_counts)
+      <li class="had"><a class="had" title="had (<?php echo $had_count; ?>)">had <span class="count"><?php if ( $had_count > 0 && $show_counts )
   echo '(' . $had_count . ')'; ?></span></a></li>
     </ul>
   </div>
@@ -15,20 +15,20 @@
   <div class="gdgt-widget-list">
     <ul class="have-list gadget-list">
       <?php
-      if (!empty($data) && is_array($data->have)) {
-        foreach ($data->have as $g) {
+      if ( ! empty( $data ) && is_array( $data->have ) ) {
+        foreach ( $data->have as $g ) {
           ?>
           <li class="list-item">
-            <a href="<?php echo esc_url(GDGT_URL . $g->url, array('http', 'https')); ?>" title="<?php echo esc_attr($g->name); ?>">
-              <img src="<?php echo esc_url(GDGT_IMG_URL . $g->image, array('http', 'https')); ?>" alt="<?php echo esc_attr($g->name); ?>" />
-              <span><?php echo esc_html($g->name); ?></span>
+            <a href="<?php echo esc_url( GDGT_URL . $g->url, array( 'http', 'https' ) ); ?>" title="<?php echo esc_attr( $g->name ); ?>">
+              <img src="<?php echo esc_url( GDGT_IMG_URL . $g->image, array( 'http', 'https' ) ); ?>" alt="<?php echo esc_attr( $g->name ); ?>" />
+              <span><?php echo esc_html( $g->name ); ?></span>
             </a>
           </li>
           <?php
         }
       } else {
         ?>
-        <li class="message"><?php echo sprintf(__('%s has made their gadget list private.', 'gdgt-gadget-list-widget'), esc_html($username)); ?></li>
+        <li class="message"><?php echo sprintf( __( '%s has made their gadget list private.', 'gdgt-gadget-list-widget' ), esc_html( $username ) ); ?></li>
   <?php
 }
 ?>
@@ -36,20 +36,20 @@
 
     <ul class="want-list gadget-list">
       <?php
-      if (!empty($data) && is_array($data->want)) {
-        foreach ($data->want as $g) {
+      if ( ! empty( $data ) && is_array( $data->want ) ) {
+        foreach ( $data->want as $g ) {
           ?>
           <li class="list-item">
-            <a href="<?php echo esc_url(GDGT_URL . $g->url, array('http', 'https')); ?>" title="<?php echo esc_attr($g->name); ?>">
-              <img src="<?php echo esc_url(GDGT_IMG_URL . $g->image, array('http', 'https')); ?>" alt="<?php echo esc_attr($g->name); ?>" />
-              <span><?php echo esc_html($g->name); ?></span>
+            <a href="<?php echo esc_url( GDGT_URL . $g->url, array( 'http', 'https' ) ); ?>" title="<?php echo esc_attr( $g->name ); ?>">
+              <img src="<?php echo esc_url( GDGT_IMG_URL . $g->image, array( 'http', 'https' ) ); ?>" alt="<?php echo esc_attr( $g->name ); ?>" />
+              <span><?php echo esc_html( $g->name ); ?></span>
             </a>
           </li>
           <?php
         }
       } else {
         ?>
-        <li class="message"><?php echo sprintf(__('%s has made their gadget list private.', 'gdgt-gadget-list-widget'), esc_html($username)); ?></li>
+        <li class="message"><?php echo sprintf( __( '%s has made their gadget list private.', 'gdgt-gadget-list-widget' ), esc_html( $username ) ); ?></li>
   <?php
 }
 ?>
@@ -57,20 +57,20 @@
 
     <ul class="had-list gadget-list">
       <?php
-      if (!empty($data) && is_array($data->had)) {
+      if ( ! empty( $data ) && is_array( $data->had ) ) {
         foreach ($data->had as $g) {
           ?>
           <li class="list-item">
-            <a href="<?php echo esc_url(GDGT_URL . $g->url, array('http', 'https')); ?>" title="<?php echo esc_attr($g->name); ?>">
-              <img src="<?php echo esc_url(GDGT_IMG_URL . $g->image, array('http', 'https')); ?>" alt="<?php echo esc_attr($g->name); ?>" />
-              <span><?php echo esc_html($g->name); ?></span>
+            <a href="<?php echo esc_url( GDGT_URL . $g->url, array( 'http', 'https' ) ); ?>" title="<?php echo esc_attr( $g->name ); ?>">
+              <img src="<?php echo esc_url( GDGT_IMG_URL . $g->image, array( 'http', 'https' ) ); ?>" alt="<?php echo esc_attr( $g->name ); ?>" />
+              <span><?php echo esc_html( $g->name ); ?></span>
             </a>
           </li>
           <?php
         }
       } else {
         ?>
-        <li class="message"><?php echo sprintf(__('%s has made their gadget list private.', 'gdgt-gadget-list-widget'), esc_html($username)); ?></li>
+        <li class="message"><?php echo sprintf( __( '%s has made their gadget list private.', 'gdgt-gadget-list-widget' ), esc_html( $username ) ); ?></li>
   <?php
 }
 ?>
@@ -78,14 +78,14 @@
   </div>
 
   <div class="gdgt-widget-footer">
-    <a href="<?php echo esc_url(GDGT_URL, array('http', 'https')); ?>/widgets/" class="create-list" title="<?php esc_attr(__('make your own list!', 'gdgt-gadget-list-widget')); ?>">// <?php echo esc_html(__('make your own list!', 'gdgt-gadget-list-widget')); ?></a>
+    <a href="<?php echo esc_url( GDGT_URL, array( 'http', 'https' ) ); ?>/widgets/" class="create-list" title="<?php esc_attr( __( 'make your own list!', 'gdgt-gadget-list-widget' ) ); ?>">// <?php echo esc_html( __( 'make your own list!', 'gdgt-gadget-list-widget' ) ); ?></a>
   </div>
 </div>
 
 <script type="text/javascript"><?php
 $params = array('user' => strtolower($username), 'element' => '#gdgt_widget_' . $username);
 foreach (array('width', 'height', 'showcount') as $key) {
-  if (isset($options[$key]))
-    $params[$key] = $options[$key];
+	if ( isset( $options[$key] ) )
+		$params[$key] = $options[$key];
 }
-?>gdgt.gadgetListWidget(<?php echo json_encode($params); ?>);</script>
+?>gdgt.gadgetListWidget(<?php echo json_encode( $params ); ?>);</script>
